72b14c3df4bb90a3a1ed5531cbbcae5f9b439596
Alan Chambers <alan.chambers@cambridgeconsultants.com>
2020-01-07 15:27:43 +0000

    Modified README.md and yagl_version.py

--- README.md
+++ README.md
@@ -95,6 +95,7 @@ The following header only libraries are included in the source tree along with t
 - **png++**: a C++ wrapper around the libpng API: https://www.nongnu.org/pngpp/.
 - **cxxopts**: a C++ command line option parser: https://github.com/jarro2783/cxxopts.
 
+Aside from **g++**, the build also makes use of **python** for a pre-build step to create the version number from the git repository: it runs `yagl_version.py` to create `yagl_version.h`. I found it necessary to manually create the link `/usr/bin/python` to the binary `/usr/bin/python3`. This may or may not be an issue on your system. 
 
 ## Licence
 
--- yagl_version.py
+++ yagl_version.py
@@ -1,4 +1,23 @@
-# Possibly replace this file with bash script or something to remove
+# ///////////////////////////////////////////////////////////////////////////////
+# // Copyright 2019 Alan Chambers (unicycle.bloke@gmail.com)
+# //
+# // This file is part of yagl.
+# //
+# // yagl is free software: you can redistribute it and/or modify
+# // it under the terms of the GNU General Public License as published by
+# // the Free Software Foundation, either version 3 of the License, or
+# // (at your option) any later version.
+# //
+# // yagl is distributed in the hope that it will be useful,
+# // but WITHOUT ANY WARRANTY; without even the implied warranty of
+# // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# // GNU General Public License for more details.
+# //
+# // You should have received a copy of the GNU General Public License
+# // along with yagl. If not, see <https://www.gnu.org/licenses/>.
+# ///////////////////////////////////////////////////////////////////////////////
+
+# TODO Possibly replace this file with bash script or something to remove
 # build dependency on Python.
 
 import os
@@ -7,9 +26,14 @@ stream = os.popen('git describe')
 version = stream.read().strip()
 
 # This is supposed to place version.h at the project top level - the parent
-# folder of the build folder. 
+# folder of the build folder. Just having an include might possibly fall foul of
+# the one definition rule, but it seems to be OK so far. Maybe using constexpr makes
+# this a non-issue? Easy to fix if it becomes a problem.
+
+# TODO compare contents with existing file to avoid unnecessary recompilation? Seems
+# to work anyway...
 outfile = open("../yagl_version.h", "w+")
-outfile.write("// This file is generated in a pre-build step by version.py\n")
+outfile.write("// This file is generated in a pre-build step by yagl_version.py\n")
 outfile.write("#pragma once\n")
 outfile.write("constexpr const char* str_yagl_version = \"%s\";" % (version))
 
