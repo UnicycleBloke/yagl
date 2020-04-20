a2e353c6ff05339f5a88440a3a630dcc36952458
Alan Chambers <alan.chambers@cambridgeconsultants.com>
2020-04-20 17:04:41 +0100

    Tests for Vehicles and Ships

--- tests/features/Test_Action00_Ships.cpp
+++ tests/features/Test_Action00_Ships.cpp
@@ -23,18 +23,75 @@
 
 namespace {
 
-// TODO Add all properties, even with silly values, so that we can confirm the 
+// Add all properties, even with silly values, so that we can confirm the 
 // formatting of each one if they are individually changed.
-static constexpr const char* str_YAGL = "";
+static constexpr const char* str_YAGL =
+    "properties<Ships, 0x0023> // Action00\n"
+    "{\n"
+    "    // instance_id: 0x0023\n"
+    "    {\n"
+    "        sprite_id: 0x34;\n"
+    "        is_refittable: true;\n"
+    "        cost_factor: 0x24;\n"
+    "        speed_2_kmh: 0x25;\n"
+    "        cargo_type: 0x36;\n"
+    "        cargo_capacity: 0x1002;\n"
+    "        running_cost_factor: 0x45;\n"
+    "        sound_effect_type: 0x32;\n"
+    "        refit_cargo_types: 0x01020304;\n"
+    "        callback_flags_mask: 0x13;\n"
+    "        refit_cost: 0x14;\n"
+    "        ocean_speed_fraction: 0x15;\n"
+    "        canal_speed_fraction: 0x16;\n"
+    "        retire_vehicle_early: 0x17;\n"
+    "        miscellaneous_flags: 0x18;\n"
+    "        refittable_cargo_classes: 0x1030;\n"
+    "        non_refittable_cargo_classes: 0x1040;\n"
+    "        long_introduction_date: date(1342/10/23);\n"
+    "        sort_purchase_list: 0x0103;\n"
+    "        visual_effect: 0x37;\n"
+    "        custom_cargo_aging_period: 0x1234;\n"
+    "        always_refittable_cargos: [ 0x01 0x03 0x12 ];\n"
+    "        never_refittable_cargos: [ 0x02 0x04 0x13 ];\n"
+    "    }\n"
+    "}\n";
 
 // NFO matching the YAGL.
-static constexpr const char* str_NFO = "";
+static constexpr const char* str_NFO =
+//    0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
+    "00 "              // Action00
+    "02 "              // Ships
+    "17 "              // 23 properties...
+    "01 "              // ... for 1 item
+    "FF 23 00 "        // First ID 0x23 (extended byte)
+    "08 34 "           // sprite_id
+    "09 01 "           // is_refittable
+    "0A 24 "           // cost_factor
+    "0B 25 "           // speed_2_kmh
+    "0C 36 "           // cargo_type
+    "0D 02 10 "        // cargo_capacity
+    "0F 45 "           // running_cost_factor
+    "10 32 "           // sound_effect_type
+    "11 04 03 02 01 "  // refit_cargo_types
+    "12 13 "           // callback_flags_mask
+    "13 14 "           // refit_cost
+    "14 15 "           // ocean_speed_fraction
+    "15 16 "           // canal_speed_fraction
+    "16 17 "           // retire_vehicle_early
+    "17 18 "           // miscellaneous_flags
+    "18 30 10 "        // refittable_cargo_classes
+    "19 40 10 "        // non_refittable_cargo_classes
+    "1A D3 7B 07 00 "  // long_introduction_date
+    "1B FF 03 01 "     // sort_purchase_list
+    "1C 37 "           // visual_effect
+    "1D 34 12 "        // custom_cargo_aging_period
+    "1E 03 01 03 12 "  // always_refittable_cargos
+    "1F 03 02 04 13 "; // never_refittable_cargos
 
 } // namespace {
 
 
-// TEST_CASE("Action00Trains", "[actions]")
-// {
-//     test_yagl<Action00Record, 0x00>(str_YAGL, str_NFO);
-// }
-
+TEST_CASE("Action00Ships", "[features]")
+{
+    test_yagl<Action00Record, 0x00>(str_YAGL, str_NFO);
+}
--- tests/features/Test_Action00_Vehicles.cpp
+++ tests/features/Test_Action00_Vehicles.cpp
@@ -23,18 +23,83 @@
 
 namespace {
 
-// TODO Add all properties, even with silly values, so that we can confirm the 
+// Add all properties, even with silly values, so that we can confirm the 
 // formatting of each one if they are individually changed.
-static constexpr const char* str_YAGL = "";
+static constexpr const char* str_YAGL =
+    "properties<Vehicles, 0x0023> // Action00\n"
+    "{\n"
+    "    // instance_id: 0x0023\n"
+    "    {\n"
+    "        speed_2_kmh: 0x23;\n"
+    "        running_cost_factor: 0x35;\n"
+    "        running_cost_base: 0x01020304;\n"
+    "        sprite_id: 0x24;\n"
+    "        cargo_capacity: 0x25;\n"
+    "        cargo_type: 0x35;\n"
+    "        cost_factor: 0x36;\n"
+    "        sound_effect_type: 0x37;\n"
+    "        power_10_hp: 0x38;\n"
+    "        weight_quarter_tons: 0x39;\n"
+    "        speed_half_kmh: 0x3A;\n"
+    "        refit_cargo_types: 0x12345678;\n"
+    "        callback_flags_mask: 0x3B;\n"
+    "        coeff_of_tractive_effort: 0x3C;\n"
+    "        coeff_of_air_drag: 0x3D;\n"
+    "        refit_cost: 0x41;\n"
+    "        retire_vehicle_early: 0x42;\n"
+    "        miscellaneous_flags: 0x43;\n"
+    "        refittable_cargo_classes: 0x4412;\n"
+    "        non_refittable_cargo_classes: 0x4413;\n"
+    "        long_introduction_date: date(1911/3/13);\n"
+    "        sort_purchase_list: 0x0102;\n"
+    "        visual_effect: 0x45;\n"
+    "        custom_cargo_aging_period: 0x1237;\n"
+    "        shorten_vehicle: 0x46;\n"
+    "        always_refittable_cargos: [ 0x02 0x03 0x04 0x11 ];\n"
+    "        never_refittable_cargos: [ 0x05 0x06 0x07 ];\n"
+    "    }\n"
+    "}\n";
 
 // NFO matching the YAGL.
-static constexpr const char* str_NFO = "";
+static constexpr const char* str_NFO =
+//    0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
+    "00 "                // Action00
+    "01 "                // Vehicles
+    "1B "                // 27 properties...
+    "01 "                // ... for 1 item
+    "FF 23 00 "          // First ID 0x23 (extended byte)
+    "08 23 "             // speed_2_kmh
+    "09 35 "             // running_cost_factor
+    "0A 04 03 02 01 "    // running_cost_base
+    "0E 24 "             // sprite_id
+    "0F 25 "             // cargo_capacity
+    "10 35 "             // cargo_type
+    "11 36 "             // cost_factor
+    "12 37 "             // sound_effect_type
+    "13 38 "             // power_10_hp
+    "14 39 "             // weight_quarter_tons
+    "15 3A "             // speed_half_kmh
+    "16 78 56 34 12 "    // refit_cargo_types
+    "17 3B "             // callback_flags_mask
+    "18 3C "             // coeff_of_tractive_effort
+    "19 3D "             // coeff_of_air_drag
+    "1A 41 "             // refit_cost
+    "1B 42 "             // retire_vehicle_early
+    "1C 43 "             // miscellaneous_flags
+    "1D 12 44 "          // refittable_cargo_classes
+    "1E 13 44 "          // non_refittable_cargo_classes
+    "1F C1 A6 0A 00 "    // long_introduction_date
+    "20 FF 02 01 "       // sort_purchase_list
+    "21 45 "             // visual_effect
+    "22 37 12 "          // custom_cargo_aging_period
+    "23 46 "             // shorten_vehicle
+    "24 04 02 03 04 11 " // always_refittable_cargos
+    "25 03 05 06 07 ";   // never_refittable_cargos
 
 } // namespace {
 
 
-// TEST_CASE("Action00Trains", "[actions]")
-// {
-//     test_yagl<Action00Record, 0x00>(str_YAGL, str_NFO);
-// }
-
+TEST_CASE("Action00Vehicles", "[features]")
+{
+    test_yagl<Action00Record, 0x00>(str_YAGL, str_NFO);
+}
