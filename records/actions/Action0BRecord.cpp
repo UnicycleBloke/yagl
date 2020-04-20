a0d0b2bdb1159274eb857c08cbc8a069c086bd15
Alan Chambers <alan.chambers@cambridgeconsultants.com>
2020-04-20 22:38:09 +0100

    Add tests for Action0B

--- records/actions/Action0BRecord.cpp
+++ records/actions/Action0BRecord.cpp
@@ -40,14 +40,14 @@ void Action0BRecord::read(std::istream& is, const GRFInfo& info)
         // - The first 0x7B is the first parameter byte below.
         // - The second 0x7B is the second parameter byte below.
         // No other combinations are permitted.
-        m_custom_message.read(is);
+        m_message.read(is);
     }
 
     // // Can either scan the string or just check for end of input. The 
     // // built in strings can't be scanned, so...
     if (is.peek() != EOF)
     {
-        m_message_data.read(is);
+        m_data.read(is);
     }
 
     m_num_params = 0;
@@ -76,13 +76,13 @@ void Action0BRecord::write(std::ostream& os, const GRFInfo& info) const
 
     if (m_message_id == 0xFF)
     {
-        m_custom_message.write(os);
+        m_message.write(os);
     }
 
     // Bug - what if the string that was read was just a 0 termination byte?
-    if (m_message_data.length() > 0)
+    if (m_data.length() > 0)
     {
-        m_message_data.write(os);
+        m_data.write(os);
     }
 
     if (m_num_params > 0) write_uint8(os, m_param1);
@@ -90,38 +90,29 @@ void Action0BRecord::write(std::ostream& os, const GRFInfo& info) const
 }  
 
 
-// error_message<Warning, default> // Action0B <severity, language>
-// {
-//     message_id: 0x05; // {substring1} must be loaded after {substring1}.
-//     message_data: "NuTracks"; // Second substring
-// }
-
-
 namespace {
 
 
-constexpr const char* str_message_id     = "message_id";
-constexpr const char* str_custom_message = "custom_message";
-constexpr const char* str_message_data   = "message_data";
+constexpr const char* str_message        = "message";
+constexpr const char* str_data           = "data";
 constexpr const char* str_param1         = "param1";
 constexpr const char* str_param2         = "param2";
 
 // From the NewGRF specs:
-constexpr const char* str_message_00 = "\x80 requires at least TTDPatch version \x80";
-constexpr const char* str_message_01 = "\x80 is for the \x80 version of TTD."; // <data> should be "DOS" or "Windows"
-constexpr const char* str_message_02 = "\x80 is designed to be used with \x80"; // <data> should be a switchname + value, e.g. "multihead 0"
-constexpr const char* str_message_03 = "Invalid parameter for \x80: parameter \x80 ( \x7B )"; // <data> should be the switch number written out ("5")
-constexpr const char* str_message_04 = "\x80 must be loaded before \x80.";
-constexpr const char* str_message_05 = "\x80 must be loaded after \x80.";
-constexpr const char* str_message_06 = "\x80 requires OpenTTD version \x80 or better."; 
+constexpr const char* str_message_00 = "{grf_name} requires at least TTDPatch version {data}";
+constexpr const char* str_message_01 = "{grf_name} is for the {data} version of TTD."; // <data> should be "DOS" or "Windows"
+constexpr const char* str_message_02 = "{grf_name} is designed to be used with {data}"; // <data> should be a switchname + value, e.g. "multihead 0"
+constexpr const char* str_message_03 = "Invalid parameter for {grf_name}: parameter {data} ({param_num})"; // <data> should be the switch number written out ("5")
+constexpr const char* str_message_04 = "{grf_name} must be loaded before {data}.";
+constexpr const char* str_message_05 = "{grf_name} must be loaded after {data}.";
+constexpr const char* str_message_06 = "{grf_name} requires OpenTTD version {data} or better."; 
 
 
 // Fake property numbers to facilitate out of order parsing.
 const std::map<std::string, uint8_t> g_indices =
 {
-    { str_message_id,     0x01 },
-    { str_custom_message, 0x02 },
-    { str_message_data,   0x03 },
+    { str_message,        0x02 },
+    { str_data,           0x03 },
     { str_param1,         0x04 },
     { str_param2,         0x05 },
 };
@@ -138,9 +129,8 @@ const EnumDescriptorT<Action0BRecord::Severity> severity_desc =
     }
 };
 
-const IntegerDescriptorT<uint8_t> desc_message_id  { 0x01, str_message_id, UIntFormat::Hex };
-const GRFStringDescriptor         desc_message     { 0x02, str_custom_message };
-const GRFStringDescriptor         desc_message_data{ 0x03, str_message_data };
+const GRFStringDescriptor         desc_message     { 0x02, str_message };
+const GRFStringDescriptor         desc_data        { 0x03, str_data };
 const IntegerDescriptorT<uint8_t> desc_param1      { 0x04, str_param1, UIntFormat::Hex };
 const IntegerDescriptorT<uint8_t> desc_param2      { 0x05, str_param2, UIntFormat::Hex };
 
@@ -152,10 +142,10 @@ void Action0BRecord::print(std::ostream& os, const SpriteZoomMap& sprites, uint1
 {
     os << pad(indent) << RecordName(record_type()) << "<";
     os << severity_desc.value(m_severity) << ", ";
-    os << language_iso(m_language_id) << "> // Action0B <severity, language>\n";
+    os << language_iso(m_language_id) << ", ";
+    os << to_hex(m_message_id) << "> // Action0B <severity, language, message>\n";
     os << pad(indent) << "{\n";
 
-    desc_message_id.print(m_message_id, os, indent + 4);
     // Indicate the standard message, if this is one.
     const char* std_message = nullptr;
     switch (m_message_id)
@@ -170,15 +160,19 @@ void Action0BRecord::print(std::ostream& os, const SpriteZoomMap& sprites, uint1
     }
     if (std_message != nullptr)
     {
-        os << pad(indent + 4) << "// " << grf_string_to_readable_utf8(std_message) << "\n";
+        os << pad(indent + 4) << "// " << std_message << "\n";
     }
 
     if (m_message_id == 0xFF)
     {
-        desc_message.print(m_custom_message, os, indent +4);
+        desc_message.print(m_message, os, indent +4);
     }
 
-    desc_message_data.print(m_message_data, os, indent +4);
+    // Optional parameters for the message.
+    if (m_data.length() > 0)
+    {
+        desc_data.print(m_data, os, indent +4);
+    }
     if (m_num_params > 0)
     {
         desc_param1.print(m_param1, os, indent + 4);
@@ -199,6 +193,8 @@ void Action0BRecord::parse(TokenStream& is)
     severity_desc.parse(m_severity, is);
     is.match(TokenType::Comma);
     m_language_id = language_id(is.match(TokenType::Ident));
+    is.match(TokenType::Comma);
+    m_message_id = is.match_uint8();
     is.match(TokenType::CloseAngle);
 
     is.match(TokenType::OpenBrace);
@@ -213,9 +209,8 @@ void Action0BRecord::parse(TokenStream& is)
 
             switch (it->second)
             {
-                case 0x01: desc_message_id.parse(m_message_id, is); break;
-                case 0x02: desc_message.parse(m_custom_message, is); break;
-                case 0x03: desc_message_data.parse(m_message_data, is); break;
+                case 0x02: desc_message.parse(m_message, is); break;
+                case 0x03: desc_data.parse(m_data, is); break;
                 case 0x04: desc_param1.parse(m_param1, is); break;
                 case 0x05: desc_param2.parse(m_param2, is); break;
             }
--- tests/actions/Test_Action0BRecord.cpp
+++ tests/actions/Test_Action0BRecord.cpp
@@ -19,3 +19,51 @@
 #include "catch.hpp"
 #include "Test_Shared.h"
 #include "Action0BRecord.h"
+
+
+namespace {
+
+// Add all properties, even with silly values, so that we can confirm the 
+// formatting of each one if they are individually changed.
+static constexpr const char* str_YAGL =
+    "error_message<Fatal, default, 0x02> // Action0B <severity, language, message>\n"
+    "{\n"
+    "    // {grf_name} is designed to be used with {data}\n"
+    "    data: \"OpenTTD\";\n"
+    "}\n";
+// NFO matching the YAGL.
+static constexpr const char* str_NFO =
+//    0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
+    "0B "  // Action0B
+    "03 "  // Fatal
+    "7F "  // Language = default
+    "02 "  // Message ID (from a table)
+    "4F 70 65 6E 54 54 44 00 "; // Custom text: OpenTTD
+
+// Add all properties, even with silly values, so that we can confirm the 
+// formatting of each one if they are individually changed.
+static constexpr const char* str_YAGL2 =
+    "error_message<Warning, en_GB, 0xFF> // Action0B <severity, language, message>\n"
+    "{\n"
+    "    message: \"The cat sat on the {substring1}.\";\n"
+    "    data: \"mat\";\n"
+    "}\n";
+// NFO matching the YAGL.
+static constexpr const char* str_NFO2 =
+//    0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
+    "0B "  // Action0B
+    "01 "  // Warning
+    "01 "  // Language = en_GB
+    "FF "  // Message ID (from a table) - custom
+    "54 68 65 20 63 61 74 20 73 61 74 20 6F 6E 20 74 68 65 20 80 2E 00 "
+    "6D 61 74 00 ";
+
+
+} // namespace {
+
+
+TEST_CASE("Action0BRecord", "[actions]")
+{
+    test_yagl<Action0BRecord, 0x0B>(str_YAGL, str_NFO);
+    test_yagl<Action0BRecord, 0x0B>(str_YAGL2, str_NFO2);
+}
